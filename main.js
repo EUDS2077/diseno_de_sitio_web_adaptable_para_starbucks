1f3fc58794be367c4d6c27f6f23ffdb1a6f2d66b
EUDS2077 <le21081367@merida.tecnm.mx>
2025-02-20 08:57:05 -0600

    Avance_7

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // Selecciona el ícono del menú y la barra de navegación
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-
 // Agrega un evento al hacer clic en el ícono del menú
 menu.onclick = () =>
 {
@@ -10,7 +9,6 @@ menu.onclick = () =>
     // Alterna la clase 'active' en la barra de navegación para mostrarla u ocultarla
     navbar.classList.toggle('active');
 }
-
 // Evento que se activa cuando el usuario hace scroll en la página
 window.onscroll = () =>
 {
